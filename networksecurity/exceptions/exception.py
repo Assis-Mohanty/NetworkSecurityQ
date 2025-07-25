5ba055641143a32c7e485b2abfcca2e55f43bb29
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-07-25 14:31:30 +0530

    Data ingestion congig and data ingestion artifact

--- networksecurity/exceptions/exception.py
+++ networksecurity/exceptions/exception.py
@@ -9,7 +9,7 @@ class NetworkSecurityException(Exception):
         self.file_name=exc_tb.tb_frame.f_code.co_filename
     
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message[{2}] ".format(
+        return "\n Error occured in python script name \n [{0}] \n line number [{1}] \n error message \n [{2}] ".format(
             self.file_name,self.lineno,str(self.error_message)
         )
 
