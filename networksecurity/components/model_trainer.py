b652601d946a22bbbaf3450362213b36752a76c7
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-07-28 15:27:20 +0530

    adding final models

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -25,6 +25,9 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='Assis-Mohanty', repo_name='NetworkSecurityQ', mlflow=True)
+
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -103,6 +106,8 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocssor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=Network_Model)
+
+        save_object("final_model/model.pkl",best_model)
 
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,train_metric_artifact=classification_train_metrics,test_metric_artifact=classification_test_metrics)
         logging.info(f"Model trainer artifact :{model_trainer_artifact}")
