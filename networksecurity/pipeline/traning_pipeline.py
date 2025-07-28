f9ce930ed3f69415ad5b37819854d0dd68d2f14d
Assis-Mohanty <assis.mohanty.98@gmail.com>
2025-07-28 19:15:44 +0530

    fast api frontend and predict give test csv

---
+++ networksecurity/pipeline/traning_pipeline.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from networksecurity.components import data_ingestion
+from networksecurity.components import data_validation
+from networksecurity.components import data_transformation
+from networksecurity.components import model_trainer
+from networksecurity.constants import training_pipeline
+from networksecurity.logging.logger import logging
+from networksecurity.exceptions.exception import NetworkSecurityException
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
+
+from networksecurity.entity.artifact_entity import(
+    DataIngestionArtifact,
+    DataTransformationArtifact,
+    DataValidationArtifact,
+    ModelTrainerArtifact
+)
+
+from networksecurity.entity.config_entity import(
+    DataIngestionConfig,
+    DataTransformationConfig,
+    DataValidationConfig,
+    ModelTrainerConfig,
+    TrainingPipelineConfig
+)
+
+class TrainingPipeline:
+    def __init__(self):
+        self.training_pipeline_config=TrainingPipelineConfig()
+    
+    def start_data_ingestion(self):
+        try:
+            self.data_ingestion_config=DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Started data ingestion")
+            data_ingestion=DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data Ingestion completed and artifact:{data_ingestion_artifact}")
+            return data_ingestion_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def start_data_validation(self,data_ingestion_artifact:DataIngestionArtifact):
+        try:
+            self.data_validation_config=DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Started data validation")
+            data_validation=DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config)
+            data_validation_artifact=data_validation.initiate_data_validation()
+            logging.info(f"Data Validation completed and artifact:{data_validation_artifact}")
+            return data_validation_artifact
+    
+    
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def start_data_transformation(self,data_validation_artifact:DataValidationArtifact):
+        try:
+            data_transformation_config=DataTransformationConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Started data validation")
+            data_transformation=DataTransformation(data_validation_artifact=data_validation_artifact,data_transformation_config=data_transformation_config)
+            data_transformation_artifact=data_transformation.initiate_data_transformation()
+            logging.info(f"Data transofrmation completed and  artifact:{data_transformation_artifact}")
+            return data_transformation_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+
+    def start_model_training(self,data_transformation_artifact:DataTransformationArtifact):
+        try:
+            self.model_training_config=ModelTrainerConfig(training_pipeline_config=self.training_pipeline_config)
+            model_trainer=ModelTrainer(model_trainer_config=self.model_training_config,data_transformation_artifact=data_transformation_artifact)
+            model_trainer_artifact=model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact=self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact=self.start_model_training(data_transformation_artifact=data_transformation_artifact)
+            return model_trainer_artifact
+
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
